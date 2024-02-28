a77f377d99a9540ce454b2ad1f71d7ebc2ab1db8
JhonatanRios <jhoto1234@gmail.com>
2024-02-28 12:42:43 -0500

    Configuración de proyecto - first comment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lista de estudiantes</h1>
+      <Form/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
